d6c8cbc0c0e85ed0bd825c40c2dc7da430a1e009
Asutosh Palai <asupalai@gmail.com>
2015-03-07 19:24:06 +0530

    First setup

---
+++ README.md
@@ -0,0 +1,8 @@
+# gitlab-contoller
+This has been created to automate the task of handling ssh keys for gitlab by admin
+
+#Requirement
+requests
+
+pip install requests
+
---
+++ run.py
@@ -0,0 +1,93 @@
+#!/bin/python
+import conf
+import requests
+
+def get( url, load = {}):
+    load['private_token'] = conf.token
+    r = requests.get(conf.base_url + url, params = load)
+    return r.json()
+
+def post( url, load = {}):
+    load['private_token'] = conf.token
+    r = requests.post(conf.base_url + url, params = load)
+    return r.json()
+
+def delete( url, load = {}):
+    load['private_token'] = conf.token
+    r = requests.delete(conf.base_url + url, params = load)
+    return r.json()
+
+
+
+
+class Controller:
+    """A class to run our custom commands"""
+
+    def __init__(self, url = conf.base_url, token = conf.token):
+        self.url = url
+        self.token = token
+
+
+    def getuser(self):
+        if hasattr(self, 'user'):
+            return self.user
+        self.user = get('/user')
+
+        return self.user
+
+
+
+    def selfusername(self):
+        if hasattr(self, 'user'):
+            return self.user['username']
+
+        return self.getuser()['username']
+
+    def getuserlist(self):
+        if hasattr(self, 'userlist'):
+            return self.userlist
+
+        self.userlist = get('/users')
+        return self.userlist
+
+    def finduser(self, username):
+        userlist = self.getuserlist()
+
+        for user in userlist:
+            if (user['username'] == username):
+                return user
+        return -1
+
+    def addsshkey(self, username, sshkey):
+        user = self.finduser(username)
+
+        res = post('/users/' + str(user['id']) + '/keys',{'id': user['id'], 'title': 'username', 'key': sshkey})
+        return res
+
+    def listsshkeys(self, username):
+        user = self.finduser(username)
+
+        res = get('/users/' + str(user['id']) + '/keys',{'uid': user['id']})
+        return res
+
+    def deletekey(self, uid, keyid):
+        res = delete('/users/' + str(uid) + '/keys/' + str(keyid), {'uid': uid, 'id':keyid})
+        return res
+
+
+    def deleteallkeys(self, username):
+        keylist = self.listsshkeys(username)
+        user = self.finduser(username)
+
+        for key in keylist:
+            self.deletekey(user['id'], key['id'])
+
+
+    def modifykeys(self, username, key):
+        self.deleteallkeys(username)
+        self.addsshkey(username, key)
+
+
+
+
+
