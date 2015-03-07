f984fb3647ff811685819f47d159d398566b93e5
Asutosh Palai <asupalai@gmail.com>
2015-03-07 20:34:37 +0530

    Update run.py

--- run.py
+++ run.py
@@ -4,44 +4,44 @@ import sys
 import base
 
 
-con = base.Controller()
-first = sys.argv[0]
+if(len(sys.argv) < 2):
+    print """Available commands are:
+    addkey <username> <key>
+    removekey <username>
+    changekey <username> <key>"""
 
+first = sys.argv[1]
+con = base.Controller()
+print first
 if (first == 'addkey'):
-    if(len(sys.argv) != 3):
+    if(len(sys.argv) != 4):
         print "Incorrect number of parameters"
         sys.exit(1)
-    user = sys.argv[1]
-    key = sys.argv[2]
+    user = sys.argv[2]
+    key = sys.argv[3]
     print "Adding key for " + user
 
     con.addsshkey(user, key)
     sys.exit()
 
 elif (first == 'removekey'):
-    if(len(sys.argv) != 2):
+    if(len(sys.argv) != 3):
         print "Incorrect number of parameters"
         sys.exit(1)
 
-    user = sys.argv[1]
+    user = sys.argv[2]
     print "Removing key for " + user
 
     con.deleteallkeys(user)
     sys.exit()
 
 elif (first == 'changekey'):
-    if(len(sys.argv) != 3):
+    if(len(sys.argv) != 4):
         print "Incorrect number of parameters"
         sys.exit(1)
-    user = sys.argv[1]
-    key = sys.argv[2]
+    user = sys.argv[2]
+    key = sys.argv[3]
     print "Changing key for " + user
 
-    con.modifykey(user, key)
+    con.modifykeys(user, key)
     sys.exit()
-else:
-    print """Available commands are:
-    addkey <username> <key>
-    removekey <username>
-    changekey <username> <key>"""
-
